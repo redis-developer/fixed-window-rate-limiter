e650f888cba3f93c72a820089d1ce2ec1d2f49aa
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2021-08-29 15:55:49 -0700

    feature: configure ReactiveRedisTemplate

--- src/main/java/com/redis/rl/FixedWindowRateLimiterApplication.java
+++ src/main/java/com/redis/rl/FixedWindowRateLimiterApplication.java
@@ -2,11 +2,20 @@ package com.redis.rl;
 
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import static org.springframework.http.MediaType.TEXT_PLAIN;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.reactive.function.server.HandlerFilterFunction;
 import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -28,6 +37,20 @@ public class FixedWindowRateLimiterApplication {
             .body(BodyInserters.fromValue("PONG")) //
         ).filter(new RateLimiterHandlerFilterFunction()).build();
   }
+
+  @Bean
+  ReactiveRedisTemplate<String, Long> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+    JdkSerializationRedisSerializer jdkSerializationRedisSerializer = new JdkSerializationRedisSerializer();
+    StringRedisSerializer stringRedisSerializer = StringRedisSerializer.UTF_8;
+    GenericToStringSerializer<Long> longToStringSerializer = new GenericToStringSerializer<>(Long.class);
+    ReactiveRedisTemplate<String, Long> template = new ReactiveRedisTemplate<>(factory,
+        RedisSerializationContext.<String, Long>newSerializationContext(jdkSerializationRedisSerializer)
+            .key(stringRedisSerializer).value(longToStringSerializer).build());
+    return template;
+  }
+
+  @Autowired
+  private ReactiveRedisTemplate<String, Long> redisTemplate;
 
   public static void main(String[] args) {
     SpringApplication.run(FixedWindowRateLimiterApplication.class, args);
