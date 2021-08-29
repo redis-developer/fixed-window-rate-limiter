f7192f3f1de59b192f1caeced2f25c3064b74a06
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2021-08-29 13:21:56 -0700

    feature: basic PING endpoint

--- src/main/java/com/redis/rl/FixedWindowRateLimiterApplication.java
+++ src/main/java/com/redis/rl/FixedWindowRateLimiterApplication.java
@@ -1,13 +1,30 @@
 package com.redis.rl;
 
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+import static org.springframework.http.MediaType.TEXT_PLAIN;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.function.BodyInserters;
 
 @SpringBootApplication
 public class FixedWindowRateLimiterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FixedWindowRateLimiterApplication.class, args);
-	}
+  @Bean
+  RouterFunction<ServerResponse> routes() {
+    return route() //
+        .GET("/api/ping", r -> ok() //
+            .contentType(TEXT_PLAIN) //
+            .body(BodyInserters.fromValue("PONG")) //
+        ).build();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(FixedWindowRateLimiterApplication.class, args);
+  }
 
 }
